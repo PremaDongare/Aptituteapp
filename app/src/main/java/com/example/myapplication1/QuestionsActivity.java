ba918e62f6dba49f1cb3db124171679b8f5056a0
Nikhildhakane <nikhildhakane27@gmail.com>
2024-02-15 18:52:01 +0530

    15/2 pull commit Nikhil

--- app/src/main/java/com/example/myapplication1/QuestionsActivity.java
+++ app/src/main/java/com/example/myapplication1/QuestionsActivity.java
@@ -113,7 +113,7 @@ public class QuestionsActivity extends AppCompatActivity {
                                 Button optionButton = (Button) optionsContainer.getChildAt(i);
                                 optionButton.setEnabled(true);
                                 optionButton.setBackgroundTintList(null);
-                                optionButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#CDB7F6")));
+                                optionButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#00FFFF")));
                             }
                         }
                     });
